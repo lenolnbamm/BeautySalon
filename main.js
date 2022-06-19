dad1feab8033e312b807c81833db961ea993d5fc
Lenoln Bryan Anselmo Melo Muniz <lenoln1997@gmail.com>
2022-06-19 00:40:47 -0300

    Correção da pasta origem do projeto.
    O projeto foi enviado em uma pasta geral de projetos, precisando subir uma pasta para acertar.

---
+++ main.js
@@ -0,0 +1,55 @@
+/*=======================  TOGGLE NAVIGATION  =======================*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*=======================  SCROLL EFFECT  =======================*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  const shadowNavOnScroll = window.scrollY >= navHeight
+
+  shadowNavOnScroll
+    ? header.classList.add('scroll')
+    : header.classList.remove('scroll')
+})
+/*=======================  SWIPER TESTIMONIALS  =======================*/
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*=======================  SCROLL REVEAL  =======================*/
+
+ScrollReveal({
+  origin: 'left',
+  distance: '30px',
+  duration: 800,
+  reset: true
+}).reveal(`
+#home, #home img, #home .title, #home p, #home .button,
+#about, #about .img, #about .title, #about p,
+#services, #services header, #services header p, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact, #contact .text, #contact .button, #contact .links`)
